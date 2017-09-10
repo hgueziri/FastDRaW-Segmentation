7dafdf12d68b40c0a6ce7236c41243c7dab0722b
Houssem-Eddine Gueziri <houssem-eddine.gueziri.1@ens.etsmtl.ca>
2017-09-10 11:25:20 -0400

    MODIF: setup.py and ignore manifest

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from distutils.core import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
   name = 'FastDRaW', 
-  version = '1.2.0',
+  version = '1.2.1',
   packages = ['FastDRaW'],
   description = 'Image segmentation algorithm using Fast Delineation by RAndom Walker',
+  long_description=readme(),
   license = 'MIT',
   author = 'Houssem-Eddine Gueziri',
   author_email = 'houssem-eddine.gueziri.1@etsmtl.net',
@@ -15,4 +20,7 @@ setup(
   				 'Intended Audience :: Developers',
     			 'License :: OSI Approved :: MIT License',
     			 'Programming Language :: Python :: 2.7'],
+  install_requires=['markdown',],
+  include_package_data = True,
+  zip_safe = False
 )
